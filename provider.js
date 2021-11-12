3d6269dd60af08f582fc69138fe2bd01bb3c3e01
nandhu-krishnan <nandhukrishnaned@gmail.com>
2021-11-12 17:56:00 +0530

    new changes

--- provider.js
+++ provider.js
@@ -1,217 +1,224 @@
-let db = require('./dbconnect')
+let db = require("./dbconnect");
 
 exports.loginProvider = (req, res) => {
-    var conLocalPool = db.conLocalPool;
+  var conLocalPool = db.conLocalPool;
 
-    let mobile = req.body.mobile;
-    let password = req.body.password;
+  let mobile = req.body.mobile;
+  let password = req.body.password;
 
-    let Response = {
-        "status": "", "message": "", data: ""
+  let Response = {
+    status: "",
+    message: "",
+    data: "",
+  };
+
+  let str = `call providerLogin('${mobile}','${password}')`;
+  conLocalPool.getConnection(function (err, con) {
+    if (err) {
+      if (con) con.release();
+      Response.status = "ERR";
+      Response.message = "Connection Error";
+      res.send(Response);
+      return;
     }
-
-    let str = `call providerLogin('${mobile}','${password}')`
-    conLocalPool.getConnection(function (err, con) {
-        if (err) {
-            if (con)
-                con.release();
-            Response.status = "ERR"; Response.message = "Connection Error"
-            res.send(Response);
-            return;
+    con.query(str, function (err, rows, fields) {
+      if (err) {
+        Response.status = "ERR";
+        Response.message = "Query Error";
+        res.send(Response);
+      } else {
+        if (rows[0][0].Result == "Failure") {
+          Response.status = "Failure";
+          Response.message = "Invalid Credentials";
+          Response.data = rows[0][0].Result;
+          res.send(Response);
+        } else {
+          Response.status = "Success";
+          Response.message = "Login Successfully";
+          Response.data = rows[0][0].Result;
+          res.send(Response);
         }
-        con.query(str, function (err, rows, fields) {
-            if (err) {
-                Response.status = "ERR"; Response.message = "Query Error"
-                res.send(Response);
-            }
-            else {
-                if (rows[0][0].Result == "Failure") {
-                    Response.status = "Failure"; Response.message = "Invalid Credentials"
-                    Response.data = rows[0][0].Result
-                    res.send(Response);
-                }
-                else {
-                    Response.status = "Success"; Response.message = "Login Successfully"
-                    Response.data = rows[0][0].Result
-                    res.send(Response);
-                }
-
-            }
-            con.release();
-
-        });
+      }
+      con.release();
     });
-}
-
+  });
+};
 
 exports.saveProvider = (req, res) => {
-    let functionname = "saveProvider"
+  let functionname = "saveProvider";
 
-    var conLocalPool = db.conLocalPool;
-    let Response = {
-        "status": "", "message": "", data: ""
+  var conLocalPool = db.conLocalPool;
+  let Response = {
+    status: "",
+    message: "",
+    data: "",
+  };
+  let nm = req.body.name;
+  let pass = req.body.password;
+  let ad = req.body.address;
+  let ph = req.body.mobile;
+  let service = req.body.service;
+  // let hourlyrate = req.body.hourlyrate
+
+  let str = `insert into provider (username,address,service,mobile,password) 
+    values ('${nm}','${ad}','${service}','${ph}','${pass}')`;
+
+  console.log(str);
+  conLocalPool.getConnection(function (err, con) {
+    if (err) {
+      if (con) con.release();
+      Response.status = "ERR";
+      Response.message = "Connection Error";
+      res.send(Response);
+      return;
     }
-    let nm = req.body.name
-    let ad = req.body.address
-    let service = req.body.service
-    let hourlyrate = req.body.hourlyrate
-
-    let str = `insert into provider (username,address,service, hourlyrate) 
-    values ('${nm}','${ad}','${service}',${hourlyrate})`
-
-    console.log(str)
-    conLocalPool.getConnection(function (err, con) {
-        if (err) {
-            if (con)
-                con.release();
-            Response.status = "ERR"; Response.message = "Connection Error"
-            res.send(Response);
-            return;
-        }
-        con.query(str, function (err, rows, fields) {
-            if (err) {
-                Response.status = "ERR"; Response.message = "Query Error"
-                res.send(Response);
-            }
-            else {
-
-                Response.status = "Success"; Response.message = "Inserted Successfully"
-                res.send(Response);
-            }
-            con.release();
-
-        });
+    con.query(str, function (err, rows, fields) {
+      if (err) {
+        Response.status = "ERR";
+        Response.message = "Query Error";
+        res.send(Response);
+      } else {
+        Response.status = "Success";
+        Response.message = "Inserted Successfully";
+        res.send(Response);
+      }
+      con.release();
     });
-}
+  });
+};
 
 exports.listProviders = (req, res) => {
-    var conLocalPool = db.conLocalPool;
-    let Response = {
-        "status": "", "message": "", data: ""
+  var conLocalPool = db.conLocalPool;
+  let Response = {
+    status: "",
+    message: "",
+    data: "",
+  };
+
+  let str = `Select * from provider where isactive =1`;
+
+  console.log(str);
+  conLocalPool.getConnection(function (err, con) {
+    if (err) {
+      if (con) con.release();
+      Response.status = "ERR";
+      Response.message = "Connection Error";
+      res.send(Response);
+      return;
     }
-
-    let str = `Select * from provider where isactive =1`
-
-    console.log(str)
-    conLocalPool.getConnection(function (err, con) {
-        if (err) {
-            if (con)
-                con.release();
-            Response.status = "ERR"; Response.message = "Connection Error"
-            res.send(Response);
-            return;
+    con.query(str, function (err, rows, fields) {
+      if (err) {
+        Response.status = "ERR";
+        Response.message = "Query Error";
+        res.send(Response);
+      } else {
+        if (rows[0][0].Result == "Failure") {
+          Response.status = "Failure";
+          Response.message = "Invalid Credentials";
+          Response.data = rows[0][0].Result;
+          res.send(Response);
+        } else {
+          Response.status = "Success";
+          Response.message = "Listed Successfully";
+          Response.data = rows;
+          res.send(Response);
         }
-        con.query(str, function (err, rows, fields) {
-            if (err) {
-                Response.status = "ERR"; Response.message = "Query Error"
-                res.send(Response);
-            }
-            else {
-
-                Response.status = "Success"; Response.message = "Listed Successfully";
-                Response.data = rows
-                res.send(Response);
-            }
-            con.release();
-
-        });
+      }
+      con.release();
     });
-}
+  });
+};
 
 exports.getMyProfile = (req, res) => {
-    var conLocalPool = db.conLocalPool;
+  var conLocalPool = db.conLocalPool;
 
-    let token = req.body.token;
+  let token = req.body.token;
 
+  let Response = {
+    status: "",
+    message: "",
+    data: "",
+  };
 
-    let Response = {
-        "status": "", "message": "", data: ""
+  let str = `call viewProfile('${token}')`;
+  conLocalPool.getConnection(function (err, con) {
+    if (err) {
+      if (con) con.release();
+      Response.status = "ERR";
+      Response.message = "Connection Error";
+      res.send(Response);
+      return;
     }
+    con.query(str, function (err, rows, fields) {
+      if (err) {
+        Response.status = "ERR";
+        Response.message = "Query Error";
+        res.send(Response);
+      } else {
+        if (rows[0].length == 1) {
+          Response.status = "Success";
+          Response.message = "Fetch Successfully";
+          Response.data = rows[0][0];
+          let photo = JSON.parse(rows[0][0].photo);
 
-    let str = `call viewProfile('${token}')`
-    conLocalPool.getConnection(function (err, con) {
-        if (err) {
-            if (con)
-                con.release();
-            Response.status = "ERR"; Response.message = "Connection Error"
-            res.send(Response);
-            return;
+          Response.data.photo = photo[0].image;
+
+          res.send(Response);
+        } else {
+          Response.status = "Failure";
+          Response.message = "Invalid Token";
+
+          res.send(Response);
         }
-        con.query(str, function (err, rows, fields) {
-            if (err) {
-                Response.status = "ERR"; Response.message = "Query Error"
-                res.send(Response);
-            }
-            else {
-                if (rows[0].length == 1) {
-                    Response.status = "Success"; Response.message = "Fetch Successfully"
-                    Response.data = rows[0][0]
-                   let photo = JSON.parse(rows[0][0].photo)
-
-                    Response.data.photo = photo[0].image;
-
-
-                    res.send(Response);
-                } else {
-                    Response.status = "Failure"; Response.message = "Invalid Token"
-
-                    res.send(Response);
-                }
-
-
-
-            }
-            con.release();
-
-        });
+      }
+      con.release();
     });
-}
+  });
+};
+//List orders with token id
+exports.orderList = (req, res) => {
+  var conLocalPool = db.conLocalPool;
 
-exports.updateProviderProfile = (req, res) => {
-    var conLocalPool = db.conLocalPool;
-    //opening the connection pool. 
-    var Response = {
-        "status": "",
-        "data": ""
+  let token = req.body.token;
+
+  let Response = {
+    status: "",
+    message: "",
+    data: "",
+  };
+
+  let str = `call orderList('${token}')`;
+  conLocalPool.getConnection(function (err, con) {
+    if (err) {
+      if (con) con.release();
+      Response.status = "ERR";
+      Response.message = "Connection Error";
+      res.send(Response);
+      return;
     }
-    //defining the return object 
-    var token = req.body.token;
-    var username = req.body.username;
-    var photo = req.body.photo;
-    var video = req.body.video;
-    var hourlyrate = req.body.hourlyrate;
+    con.query(str, function (err, rows, fields) {
+      if (err) {
+        Response.status = "ERR";
+        Response.message = "Query Error";
+        res.send(Response);
+      } else {
+        if (rows[0].length == 1) {
+          Response.status = "Success";
+          Response.message = "Fetch Successfully";
+          Response.data = rows[0][0];
+          let photo = JSON.parse(rows[0][0].photo);
 
+          Response.data.photo = photo[0].image;
 
-    let str = `call updateProviderProfile('${token}','${username}','${photo}','${video}','${hourlyrate}')`
-    console.log(str)
-    conLocalPool.getConnection(function (err, con) {
-        if (err) {
-            if (con)
-                con.release();
-            Response.status = "ERR"; Response.data = "Connection Error"
-            res.send(Response);
-            return;
+          res.send(Response);
+        } else {
+          Response.status = "Failure";
+          Response.message = "Invalid Token";
+
+          res.send(Response);
         }
-        
-        con.query(str, function (err, rows, fields) {
-            if (err) {
-                Response.status = "ERR"; Response.data = "Query Error"
-                res.send(Response);
-            } 
-            else {
-                if (rows[0][0].Result == "Failure") {
-                    Response.status = "Failure"; Response.message = "Token Failure"
-                    Response.data = rows[0][0].Result
-                    res.send(Response);
-                }
-                else {
-                    Response.status = "Success"; Response.message = "Customer Updated Successfully"
-                    Response.data = rows
-                    res.send(Response);
-                }
-            }
-            con.release();
-
-        });
+      }
+      con.release();
     });
-}
+  });
+};
